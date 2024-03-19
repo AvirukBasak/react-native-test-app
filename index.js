401ad44918439d1325297852451131c7f89e0f50
AvirukBasak <avirukbasak@gmail.com>
2024-03-19 23:09:37 +0530

    Add Google Services plugin to build.gradle files

--- index.js
+++ index.js
@@ -4,8 +4,11 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+
 import {name as appName} from './app.json';
 import firebase from '@react-native-firebase/app';
+
+AppRegistry.registerComponent(appName, () => App);
 
 const firebaseConfig = {
   apiKey: 'AIzaSyD-2Yj3dA5V8Z5z1cGk7G9J5tJ6yV3r8P8',
@@ -24,5 +27,3 @@ if (firebase.apps.length === 0) {
 
 // set up messaging
 const messaging = firebase.messaging();
-
-AppRegistry.registerComponent(appName, () => App);
